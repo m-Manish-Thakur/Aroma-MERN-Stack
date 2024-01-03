9636528832e54555b560c7aebc6695e1a06f3769
Manish Thakur <113657343+m-Manish-Thakur@users.noreply.github.com>
2024-01-03 09:56:57 +0530

    Update User.js

--- server/Routes/User.js
+++ server/Routes/User.js
@@ -1,75 +1,75 @@
-const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../Models/User");
+// const express = require("express");
+// const router = express.Router();
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const User = require("../Models/User");
 
-router.use(express.json());
+// router.use(express.json());
 
-// Register   ---------------------------------------------------------
+// // Register   ---------------------------------------------------------
 
-router.post("/register", async (req, res) => {
-  try {
-    // Checking if user is already exist or not
-    const foundUser = await User.findOne({ email: req.body.email });
+// router.post("/register", async (req, res) => {
+//   try {
+//     // Checking if user is already exist or not
+//     const foundUser = await User.findOne({ email: req.body.email });
 
-    if (!foundUser) {
-      // Hashing Password
-      let hashPassword = await bcrypt.hash(req.body.password, 10);
+//     if (!foundUser) {
+//       // Hashing Password
+//       let hashPassword = await bcrypt.hash(req.body.password, 10);
 
-      const user = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashPassword,
-      });
-      console.log(user);
-      res.status(201).json(user);
-    } else {
-      return res.status(400).json({ error: "User already exists" });
-    }
-  } catch (err) {
-    console.log(err);
-    res.send("Server Error");
-  }
-});
+//       const user = await User.create({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: hashPassword,
+//       });
+//       console.log(user);
+//       res.status(201).json(user);
+//     } else {
+//       return res.status(400).json({ error: "User already exists" });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.send("Server Error");
+//   }
+// });
 
-// Login    -----------------------------------------------------------
+// // Login    -----------------------------------------------------------
 
-router.post("/login", async (req, res) => {
-  try {
-    // Checking if user email is already exist
-    let user = await User.findOne({ email: req.body.email });
+// router.post("/login", async (req, res) => {
+//   try {
+//     // Checking if user email is already exist
+//     let user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-      return res.status(400).json({ error: "Email is not exists" });
-    }
+//     if (!user) {
+//       return res.status(400).json({ error: "Email is not exists" });
+//     }
 
-    // Checking if password is match
+//     // Checking if password is match
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+//     const validPassword = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
 
-    if (!validPassword) {
-      return res.status(400).json({ error: "Email or Password is wrong" });
-    }
+//     if (!validPassword) {
+//       return res.status(400).json({ error: "Email or Password is wrong" });
+//     }
 
-    // Generate JWT Token
-    const token = jwt.sign(
-      { _id: user._id, email: user.email },
-      "$uper$ecretKey231690"
-    );
-    res.json({ token, user });
-    console.log({ token });
-  } catch (err) {
-    console.log(err);
-  }
-});
+//     // Generate JWT Token
+//     const token = jwt.sign(
+//       { _id: user._id, email: user.email },
+//       "$uper$ecretKey231690"
+//     );
+//     res.json({ token, user });
+//     console.log({ token });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-router.get("/", async (req, res) => {
-  const user = await User.find({});
-  res.json(user);
-});
+// router.get("/", async (req, res) => {
+//   const user = await User.find({});
+//   res.json(user);
+// });
 
-module.exports = router;
+// module.exports = router;
